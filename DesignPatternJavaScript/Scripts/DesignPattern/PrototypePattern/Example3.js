6f79cc7f1afa3b20710cb5c42aedcf373e4dcf50
Andre <andremco1992@outlook.com>
2018-03-25 22:43:30 -0300

    Prototype pattern

---
+++ DesignPatternJavaScript/Scripts/DesignPattern/PrototypePattern/Example3.js
@@ -0,0 +1,22 @@
+﻿var vehiclePrototype = {
+
+    init: function (carModel) {
+        this.model = carModel;
+    },
+
+    getModel: function () {
+        console.log("The model of this vehicle is.." + this.model);
+    }
+};
+
+var beget = (function () {
+
+    function F() { }
+
+    return function (proto) {
+        F.prototype = proto;
+        return new F();
+    };
+})();
+
+var car = beget(vehiclePrototype);
