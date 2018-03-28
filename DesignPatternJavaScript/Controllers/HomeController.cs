47d9dcd46534d7ed78cf1d1ce704b5e09060dd75
Andre <andremco1992@outlook.com>
2018-03-27 23:33:10 -0300

    Status temp code

--- DesignPatternJavaScript/Controllers/HomeController.cs
+++ DesignPatternJavaScript/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,6 +11,8 @@ namespace DesignPatternJavaScript.Controllers
     {
         public ActionResult Index()
         {
+            var status = GerarStatusParaPesquisa(true, false, false);
+
             return View();
         }
 
@@ -29,9 +32,43 @@ namespace DesignPatternJavaScript.Controllers
 
         public ActionResult ObserverPattern()
         {
+            //Javascript pattern
             ViewBag.Message = "Your contact page.";
 
             return View();
         }
+
+        //Código Temp!!
+        private string[] GerarStatusParaPesquisa(bool emProcessamento, bool recebidoSemErros, bool recebidoComErros)
+        {
+            string[] status = new string[3];
+
+            if (emProcessamento) status[0] = StatusArquivo.PRO.ToString();
+            if (recebidoSemErros) status[1] = StatusArquivo.SEM.ToString();
+            if (recebidoComErros) status[2] = StatusArquivo.COM.ToString();
+
+            if (status.All(s => s == null))
+            {
+                status = new string[3] 
+                {
+                    StatusArquivo.PRO.ToString(),
+                    StatusArquivo.SEM.ToString(),
+                    StatusArquivo.COM.ToString()
+                };
+            }
+
+            return status;
+        }
+
+    }
+
+    public enum StatusArquivo
+    {
+        [Description("Em processamento")]
+        PRO,
+        [Description("Recebido sem erros")]
+        SEM,
+        [Description("Recebido com erros")]
+        COM,
     }
 }
